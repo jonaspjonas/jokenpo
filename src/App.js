a477a81045e485073898db476ab55d3b0be65e4e
JONAS OLIVEIRA <jonas.oliveira@GASPARVIANNA.PA.GOV.BR>
2022-01-09 18:40:07 -0300

    inicio do projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import goku from './assets/goku.jpg';
+import vegeta from './assets/vegeta.jpg';
+import pedra from './assets/pedra.png';
+import papel from './assets/papel.png';
+import tesoura from './assets/tesoura.png';
+import { useState } from 'react';
 
 function App() {
+
+  const [escolhaPlayer, setEscolhaPlayer] = useState();
+  const [escolhaPc, setEscolhaPc] = useState();
+  const [resultado, setResultado] = useState(" ");
+
+  function jogar() {
+    console.log("entrou");
+    if(!escolhaPlayer) {
+      alert("Por favor, escolha uma jogada!");
+      return;
+    }
+
+    let sorteio = Math.random();
+    sorteio = Math.floor(sorteio * 3);
+
+    let escolha = Array();
+    escolha[0] = pedra;
+    escolha[1] = papel;
+    escolha[2] = tesoura;
+
+    setEscolhaPc(escolha[sorteio]);
+
+    if (escolhaPlayer === pedra && escolhaPc === tesoura || escolhaPlayer === papel && escolhaPc == pedra || escolhaPlayer === tesoura && escolhaPc == papel) {
+      setResultado("You Win!");
+    }
+    if (escolhaPc === pedra && escolhaPlayer === tesoura || escolhaPc === papel && escolhaPlayer == pedra || escolhaPc === tesoura && escolhaPlayer == papel) {
+      setResultado("You Loose!");
+    }
+    if (escolhaPlayer === pedra && escolhaPc === pedra || escolhaPlayer === papel && escolhaPc == papel || escolhaPlayer === tesoura && escolhaPc == tesoura) {
+      setResultado("Draw");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        <img src={goku} alt="cabeça goku" />
+        <h1>JOKENPO</h1>
+        <img src={vegeta} alt="cabeça vegeta" />
       </header>
+
+      <main>
+        <h1>Rodada 1</h1>
+
+        <div className="app-game">
+          <div className="app-player">
+            <h2>Esolha sua jogada</h2>
+            <img src={pedra} alt="mão fechada" onClick={()=> setEscolhaPlayer(pedra)}/>
+            <img src={papel} alt="mão aberta" onClick={()=> setEscolhaPlayer(papel)} />
+            <img src={tesoura} alt="mão em forma de tesoura" onClick={()=> setEscolhaPlayer(tesoura)} />
+            <h2>Sua escolha</h2>
+            <img src={escolhaPlayer} alt="" />
+          </div>
+
+          <button className="app-button" onClick={()=> jogar()}>Jogar</button>
+
+          <div className="app-computer">
+            <h2>Jogada do PC</h2>
+            <img src={escolhaPc} alt="" />
+          </div>
+        </div>
+
+        <h1>*{resultado}*</h1>
+
+      </main>
+
+
     </div>
   );
 }
